2c3d5969315534c5f538da3cebc8f966ffa1b1c8
Flashscope <flashscope@limecolor.com>
2014-01-25 17:39:25 +0900

    handler등록을 xml파일을 파싱해서 사용

--- src/ServerInitializer.java
+++ src/ServerInitializer.java
@@ -1,6 +1,16 @@
 
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.XMLEvent;
+
 
 /**
  * @author Michael J.Donahoo, Kenneth L.Calvert
@@ -20,14 +30,49 @@ public class ServerInitializer {
         serverInitializer.initializeServer(servPort);
     }
 
-    public void initializeServer(int servPort) throws IOException {
+    public void initializeServer(int servPort) throws IOException, InstantiationException, IllegalAccessException, ClassNotFoundException {
     	System.out.println("Server ON");
     	
+    	ArrayList<String> handlerList = getHandlerList();
+    	
     	Reactor reactor = new Reactor(servPort);
-
-        reactor.registerHandler(new StreamSayHelloEventHandler());
-        reactor.registerHandler(new StreamUpdateProfileEventHandler());
+    	
+        for (int i = 0; i < handlerList.size(); i++) {
+        	reactor.registerHandler( (EventHandler) Class.forName(handlerList.get(i)).newInstance());
+		}
 
         reactor.handle_events();
     }
+    
+	private ArrayList<String> getHandlerList() {
+		ArrayList<String> handlerList = new ArrayList<String>();
+		try {
+			XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+			InputStream in = new FileInputStream("handlerList.xml");
+			XMLEventReader eventReader = inputFactory.createXMLEventReader(in);
+			XMLEvent event;
+			String nodeName = "";
+			while (eventReader.hasNext()) {
+				event = eventReader.nextEvent();
+
+				if (event.isStartElement()) {
+					nodeName = event.asStartElement().getName().toString();
+				} else if (nodeName.equals("handler") && event.isCharacters()) {
+					handlerList.add(event.asCharacters().getData());
+				}
+				
+				if (event.isEndElement()) {
+					nodeName = "";
+				}
+
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (XMLStreamException e) {
+			e.printStackTrace();
+		}
+		
+		return handlerList;
+
+	}
 }
